db696b0bf99ec6e8773309e4d4a379986dff31fc
kashi <mbsurya19122004@gmail.com>
2025-03-19 14:56:39 +0530

    added requirement

--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\M B Suryanarayanan\VSCODE FOLDERS\PROJECTS\PERSONAL\auction site\backend\venv
+set VIRTUAL_ENV=C:\Users\M B Suryanarayanan\OneDrive\VSCODE FOLDERS\PROJECTS\PERSONAL\aucrade\aucrade\backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
